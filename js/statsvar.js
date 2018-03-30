c1b4e240408a4c89f113d10c8f97d4a9cf74466d
Gazzia29 <36985411+Gazzia29@users.noreply.github.com>
2018-03-30 12:26:04 +0200

    Disc>Agi Actualisation stats race

--- js/statsvar.js
+++ js/statsvar.js
@@ -1,34 +1,34 @@
 var classeXForce = localStorage.classeXForce;
 var classeXMag = localStorage.classeXMag;
 var classeXChar = localStorage.classeXChar;
-var classeXDisc = localStorage.classeXDisc;
+var classeXAgi = localStorage.classeXAgi;
 
 var raceXForce = localStorage.raceXForce;
 var raceXMag = localStorage.raceXMag;
 var raceXChar = localStorage.raceXChar;
 var raceXDex = localStorage.raceXDex;
-var raceXDisc = localStorage.raceXDisc;
+var raceXAgi = localStorage.raceXAgi;
 var raceXInt = localStorage.raceXInt;
 /**/
 var bForce = localStorage.bForce;
 var bMag = localStorage.bMag;
 var bChar = localStorage.bChar;
 var bDex = localStorage.bDex;
-var bDisc = localStorage.bDisc;
+var bAgi = localStorage.bAgi;
 var bInt = localStorage.bInt;
 /**/
 var totalForce = Math.round((+raceXForce * +classeXForce) * +bForce);
 var totalMag = Math.round((+raceXMag * +classeXMag) * +bMag);
 var totalChar = Math.round((+raceXChar * +classeXChar) * +bChar);
 var totalDex = Math.round(+raceXDex * +bDex);
-var totalDisc = Math.round((+raceXDisc * +classeXDisc) * +bDisc);
+var totalAgi = Math.round((+raceXAgi * +classeXAgi) * +bAgi);
 var totalInt = Math.round(+raceXInt * +bInt);
 
 var armeSelected = localStorage.armeSelected;
-if(armeSelected=="Poing"){ 
+if(armeSelected=="Poing"){
 	localStorage.setItem("dmgbasemin", "0");
 	localStorage.setItem("dmgbasemax", "0");}
-if(armeSelected=="Baton"){ 
+if(armeSelected=="Baton"){
 	localStorage.setItem("dmgbasemin", "0");
 	localStorage.setItem("dmgbasemax", "10");}
 var maxdmg = localStorage.dmgbasemax;
