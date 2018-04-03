5c2c1b7fe6b74955b8f7f0dbe51fbc159f2b8659
Gazzia29 <36985411+Gazzia29@users.noreply.github.com>
2018-04-03 15:29:44 +0200

    Inventaire last?

---
+++ js/itemsdata.js
@@ -0,0 +1,60 @@
+//ARMES
+var Poings = {
+  Type: "arme",
+  Name: "Poings",
+  StatShort: "0Nor",
+  Stat: "Dmg Max : 0 Normal",
+  Desc: "Vous ne possédez pas d'arme, et vos maigres poings peu habitués au corps à corps ne vous serviront à presque rien.",
+  Img: "weapons/fist.png"
+};
+var Baton = {
+  Type: "arme",
+  Name: "Branche de Tilleul",
+  StatShort: "10Nor",
+  Stat: "Dmg Max : 10 Normal",
+  Desc: "Une arme rudimentaire. Cette branche pourra peut-être asséner quelques bleus à un adversaire, qui sait?.",
+  Img: "weapons/stick.png"
+};
+
+//HEAD
+var Tete = {
+  Type: "head",
+  Name: "Tête",
+  StatShort: "0Nor",
+  Stat: "Def Max : 0 Normal",
+  Desc: "Vous n'avez aucune protection à la tête.",
+  Img: "armor/head.png"
+};
+//TORSE
+var Torse = {
+  Type: "torse",
+  Name: "Torse",
+  StatShort: "0Nor",
+  Stat: "Def Max : 0 Normal",
+  Desc: "Vous n'avez aucune protection au torse.",
+  Img: "armor/torso.png"
+};
+//JAMBES
+var Jambes = {
+  Type: "leg",
+  Name: "Jambes",
+  StatShort: "0Nor",
+  Stat: "Def Max : 0 Normal",
+  Desc: "Vous n'avez aucune protection aux jambes.",
+  Img: "armor/legs.png"
+}
+//PIEDS
+var Pieds = {
+  Type: "foot",
+  Name: "Pieds",
+  StatShort: "0Nor",
+  Stat: "Def Max : 0 Normal",
+  Desc: "Vous n'avez aucune protection aux pieds.",
+  Img: "armor/barefoot.png"
+}
+//TOOLS
+var Pelle = {
+  Name: "Pelle",
+  Desc: "Une vieille pelle.",
+}
+//OBJETS
