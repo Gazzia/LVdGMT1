7e125f0b93902d9ce1126223b0abb82c3f09a856
Gazzia29 <36985411+Gazzia29@users.noreply.github.com>
2018-04-06 00:36:46 +0200

    New combat UI - Day 1

---
+++ js/data/classes.js
@@ -0,0 +1,11 @@
+//ARMES
+var Rat_5 = {
+  Type: "animal",
+  Name: "Rat",
+  DmgBase: 10,
+  DmgCoef: 2.5,
+  DefBase: 0,
+  DefCoef: 1,
+  HealthBase: 5,
+  HealthCoef: 2.5
+};
--- js/data/enemies.js
+++ js/data/enemies.js
@@ -1,5 +1,24 @@
 //ARMES
-var Rat_5 = {
+var Rat = {
   Type: "animal",
   Name: "Rat",
+  Desc: "Un gros rat en colère. Des dents vous crèvent la peau à chaque morsure de l'animal.",
+  DmgBaseNor: 10,
+  DmgCoefNor: 2.5,
+  DefBaseNor: 0,
+  DefCoefNor: 1,
+  HealthBase: 10,
+  HealthCoef: 2.5
+};
+//ARMES
+var Test = {
+  Type: "animal",
+  Name: "Test.Name",
+  Desc: "Mdrr Test.Desc",
+  DmgBaseNor: 13,
+  DmgCoefNor: 2.2,
+  DefBaseNor: 0,
+  DefCoefNor: 1,
+  HealthBase: 18,
+  HealthCoef: 5.2
 };
