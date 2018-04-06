f837d28fbdd9146830b8affbcfb074c054ab0da9
Gazzia29 <36985411+Gazzia29@users.noreply.github.com>
2018-04-06 20:37:40 +0200

    Combat day3

--- js/data/enemies.js
+++ js/data/enemies.js
@@ -5,9 +5,9 @@ var Rat = {
   Desc: "Un gros rat en colère. Des dents vous crèvent la peau à chaque morsure de l'animal.",
   DmgBaseNor: 10,
   DmgCoefNor: 2.5,
-  DefBaseNor: 0,
-  DefCoefNor: 1,
-  HealthBase: 10,
+  DefBaseNor: 2,
+  DefCoefNor: 2.5,
+  HealthBase: 14,
   HealthCoef: 2.5
 };
 //ARMES
